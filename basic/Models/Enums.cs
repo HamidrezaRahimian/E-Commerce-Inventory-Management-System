60d7a3adf7288f5ad2dd05c6a6e0c1971aee9dc1
Alex <hamidrezarahimian15@gmail.com>
2025-10-31 18:37:19 +0100

    delte error

--- basic/Models/Enums.cs
+++
@@ -1,7 +0,0 @@
-namespace ECommerceInventorySystem.Models
-{
-    public enum ProductCategory { Electronics, Clothing, Food, HomeGarden, Sports, Books }
-    public enum StockStatus { InStock, LowStock, OutOfStock, Discontinued, Backordered }
-    public enum WarehouseLocation { MainWarehouse, RegionalCenter, ReturnCenter }
-    public enum ProductCondition { New, Refurbished, OpenBox, Damaged }
-}
--- basic/Models/IStorable.cs
+++
@@ -1,8 +0,0 @@
-namespace ECommerceInventorySystem.Models
-{
-    public interface IStorable
-    {
-        string SKU { get; }
-        int Quantity { get; set; }
-    }
-}
